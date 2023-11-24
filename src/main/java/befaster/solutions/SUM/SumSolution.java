3c2b6158d19c2444c60932901c1ed352921d23ec
sbx_user1051 <sbx_user1051@169.254.31.185>
2023-11-24 14:32:40 +0000

    Fri Nov 24 14:32:40 UTC 2023

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -5,8 +5,14 @@ import befaster.runner.SolutionNotImplementedException;
 @SuppressWarnings("unused")
 public class SumSolution {
 
+    public int sol (int x, int y) {
+        return x + y;
+    }
+
+
     public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+        return sol(x, y);
     }
 
 }
+
