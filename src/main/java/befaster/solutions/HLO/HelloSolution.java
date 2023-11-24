7958338e7fd5aa7b5e256c2128805479dc6cfece
sbx_user1051 <sbx_user1051@169.254.31.185>
2023-11-24 14:37:54 +0000

    Fri Nov 24 14:37:54 UTC 2023

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -8,10 +8,11 @@ public class HelloSolution {
     }
 
     public String hello2(String friendName) {
-        return "Hello, " + friendName;
+        return "Hello, " + friendName + "!";
     }
     public String hello(String friendName) {
         return hello2(friendName);
     }
 }
+
 
